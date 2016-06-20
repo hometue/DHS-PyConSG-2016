a41ffe4ae4a9a76c1dc0b1d8b2006b5484546047
hometue <hometue.lee@gmail.com>
2016-06-20 13:12:25 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,18 +1,18 @@
 import os
 from shutil import move
 
-music_dir="./music"
-img_dir="./image"
-doc_dir="./documents"
+music_dir = "./music"
+img_dir = "./image"
+doc_dir = "./documents"
 
-music_ext=["mp3"]
-img_ext=["jpg", "png"]
-doc_ext=["doc", "docx", "ppt", "pptx", "xls", "xlsx"]
+music_ext = ["mp3"]
+img_ext = ["jpg", "png"]
+doc_ext = ["doc", "docx", "ppt", "pptx", "xls", "xlsx"]
 
-def getextension(x):
+def get_extension(x):
     file_extension = os.path.splitext(x)[1]
     print(x)
-    file_extension=file_extension.replace(".", "")
+    file_extension = file_extension.replace(".", "")
     return file_extension
 
 if not(os.path.exists(music_dir)):
@@ -22,17 +22,17 @@ if not(os.path.exists(img_dir)):
 if not(os.path.exists(doc_dir)):
     os.mkdir(doc_dir)
 
-files=os.listdir(str('.'))
+files = os.listdir(str('.'))
 print(files)
-i=0
-while(i<len(files)):
+i = 0
+while(i < len(files)):
     print(i)
     if(os.path.isfile(files[i])):
-        ext=getextension(files[i])
+        ext = get_extension(files[i])
         if (ext in music_ext):
-            move(files[i], music_dir+"/"+files[i])
+            move(files[i], music_dir + "/" + files[i])
         elif (ext in img_ext):
-            move(files[i], img_dir+"/"+files[i])
+            move(files[i], img_dir + "/" + files[i])
         elif (ext in doc_ext):
-            move(files[i], doc_dir+"/"+files[i])
-    i=i+1
+            move(files[i], doc_dir + "/" + files[i])
+    i = i+1
